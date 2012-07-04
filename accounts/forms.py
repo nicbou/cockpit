baba551fc6bf497f50b42292ffefaba03d5dbb26
Nicolas Bouliane <contact@nicolasbouliane.com>
2012-07-04 17:26:14 -0400

    Made the email field mandatory

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,9 @@ from django import forms
 from django.forms import ModelForm
 
 class UserProfileForm(ModelForm):
+	def __init__(self, *args, **kwargs):
+		super(UserProfileForm, self).__init__(*args, **kwargs)
+		self.fields['email'].required = True
 	class Meta:
 		model = User
 		fields=('first_name', 'last_name', 'email')
