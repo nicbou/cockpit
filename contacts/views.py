ff1e86b3b8efdaf9710596d4f165ef92eeecaf75
Nicolas Bouliane <contact@nicolasbouliane.com>
2012-07-14 15:19:13 -0400

    Added the new user form to the index view

--- contacts/views.py
+++ contacts/views.py
@@ -1,4 +1,5 @@
 from contacts.models import *
+from contacts.forms import *
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
@@ -6,16 +7,45 @@ from django.http import HttpResponseRedirect
 
 @login_required()
 def contact_list(request):
-	contacts = Contact.objects.filter(company_id = request.user.get_profile().company.id)
-		
+	company_id = request.user.get_profile().company.id
+	contacts = Contact.objects.filter(company_id = company_id)
+	
+	PhoneNumberFormSet = inlineformset_factory(Contact, PhoneNumber, can_delete=True)
+	EmailAddressFormSet = inlineformset_factory(Contact, EmailAddress, can_delete=True)    
+	WebsiteFormSet = inlineformset_factory(Contact, Website, can_delete=True)    
+	
+	if request.POST:
+		contact = Contact()
+		contact_form = ContactAddForm(request.POST,instance=contact)
+		phonenumber_formset = PhoneNumberFormSet(request.POST,prefix='phone',instance=contact)
+		emailaddress_formset = EmailAddressFormSet(request.POST,prefix='address',instance=contact)
+		website_formset = WebsiteFormSet(request.POST,prefix='address',instance=contact)
+
+		if contact_form.is_valid() and phonenumber_formset.is_valid() and emailaddress_formset.is_valid() and website_formset.is_valid():
+			new_contact = contact_form.save(commit=False)
+			new_contact.company_id = company_id
+			new_contact.save()
+			phonenumber_formset.save()
+			emailaddress_formset.save()
+			website_formset.save()
+			return HttpResponseRedirect(reverse('contacts.views.contact_list'))
+	else:
+		contact_form = ContactAddForm()
+		phonenumber_formset = PhoneNumberFormSet(prefix='phone')
+		emailaddress_formset = EmailAddressFormSet(prefix='address')
+		website_formset = WebsiteFormSet(prefix='address')
+	
+	formset = PhoneNumberFormSet()
 	return render(request,"contacts_index.html",{
 		'contacts' : contacts,
+		'contact_form' : contact_form,
+		'phonenumber_formset' : phonenumber_formset,
+		'emailaddress_formset' : emailaddress_formset,
+		'website_formset' : website_formset,
+		'formset' : formset,
 	})
 	
 @login_required()
-def contact_single(request):
-	contacts = Contact.objects.filter(company_id = request.user.get_profile().company.id)
-		
-	return render(request,"contacts_index.html",{
-		'contacts' : contacts,
+def contact_single(request,contact_id):
+	return render(request,"contact_single.html",{
 	})
