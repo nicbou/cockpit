161d3126da6cadbe8022e44fc90e29c8d94a5e74
Nicolas Bouliane <contact@nicolasbouliane.com>
2012-07-15 10:21:36 -0400

    Added the view for viewing/editing single contacts

--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,8 @@
 from contacts.models import *
 from contacts.forms import *
+from projects.views import user_can_access_company
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
@@ -44,8 +46,42 @@ def contact_list(request):
 		'website_formset' : website_formset,
 		'formset' : formset,
 	})
-	
+		
 @login_required()
 def contact_single(request,contact_id):
+	contact = get_object_or_404(Contact,id=contact_id)
+	
+	if not user_can_access_company(request.user,contact.company_id):
+		return HttpResponse("You are not allowed to access this",status=403)
+	
+	PhoneNumberFormSet = inlineformset_factory(Contact, PhoneNumber, can_delete=False)
+	EmailAddressFormSet = inlineformset_factory(Contact, EmailAddress, can_delete=False)    
+	WebsiteFormSet = inlineformset_factory(Contact, Website, can_delete=False)    
+	
+	if request.POST:
+		contact_form = ContactAddForm(request.POST,instance=contact)
+		phonenumber_formset = PhoneNumberFormSet(request.POST,prefix='phone',instance=contact)
+		emailaddress_formset = EmailAddressFormSet(request.POST,prefix='address',instance=contact)
+		website_formset = WebsiteFormSet(request.POST,prefix='website',instance=contact)
+
+		if contact_form.is_valid() and phonenumber_formset.is_valid() and emailaddress_formset.is_valid() and website_formset.is_valid():
+			contact_form.save()
+			phonenumber_formset.save()
+			emailaddress_formset.save()
+			website_formset.save()
+			return HttpResponseRedirect(reverse('contacts.views.contact_single',args=[contact_id]))
+	else:
+		contact_form = ContactAddForm(instance=contact)
+		phonenumber_formset = PhoneNumberFormSet(instance=contact,prefix='phone')
+		emailaddress_formset = EmailAddressFormSet(instance=contact,prefix='address')
+		website_formset = WebsiteFormSet(instance=contact,prefix='website')
+	
+	formset = PhoneNumberFormSet()
 	return render(request,"contact_single.html",{
+		'contact' : contact,
+		'contact_form' : contact_form,
+		'phonenumber_formset' : phonenumber_formset,
+		'emailaddress_formset' : emailaddress_formset,
+		'website_formset' : website_formset,
+		'formset' : formset,
 	})
