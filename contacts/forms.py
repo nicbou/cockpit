1bd57da9603d0597a038353c9ac81406270ff099
Nicolas Bouliane <contact@nicolasbouliane.com>
2012-07-17 13:52:56 -0400

    Replaced the picture upload widget by an unclearable one

--- contacts/forms.py
+++ contacts/forms.py
@@ -1,9 +1,10 @@
 from contacts.models import *
 from django import forms
 from django.forms import ModelForm
-
+from django.forms import widgets
 
 class ContactAddForm(ModelForm):
+	picture = forms.ImageField(widget=widgets.FileInput)
 	class Meta:
 		model = Contact
 		exclude = ('company',)
