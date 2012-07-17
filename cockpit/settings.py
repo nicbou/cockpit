321692ba0b41c9e872ec0e481e1d1adae50987b6
Nicolas Bouliane <contact@nicolasbouliane.com>
2012-07-16 21:22:26 -0400

    Moved all uploads to the files folder, moved the current documents folder

--- cockpit/settings.py
+++ cockpit/settings.py
@@ -46,12 +46,12 @@ USE_TZ = True
 SITE_ROOT = os.path.dirname(__file__)
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = SITE_ROOT + '/../files/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/files/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
