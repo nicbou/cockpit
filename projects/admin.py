d89c4f5b5a06942f7c9786b1778d9311a8848256
Nicolas Bouliane <contact@nicolasbouliane.com>
2012-07-18 14:32:54 -0400

    Removed useless files

--- projects/admin.py
+++
@@ -1,8 +0,0 @@
-from projects.models import *
-from django.contrib import admin
-
-admin.site.register(Project)
-admin.site.register(Company)
-admin.site.register(Document)
-admin.site.register(Memo)
-admin.site.register(Task)
